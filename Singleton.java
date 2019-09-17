485ef4fd2665e69b818014b419378ee33ce97670
dm24530 <dm24530@163.com>
2019-09-17 17:27:51 +0800

    img commit

---
+++ Singleton.java
@@ -0,0 +1,16 @@
+//单例模式对象
+public class Singleton {
+    private Singleton(){
+    }
+    private static volatile Singleton instance = null;
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized(Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
